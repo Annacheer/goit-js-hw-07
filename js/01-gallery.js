e5b3ce2747e5921b97530b7047a9b0fcc7583d9b
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-08 21:35:56 +0200

    Event delegation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryList = document.querySelector('.gallery');
 
-const imagesListArr = galleryItems.map((image, index) => {
+const imagesListArr = galleryItems.map((image) => {
 return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image"
       src="${image.preview}"
       data-source="${image.original}"
@@ -17,4 +17,15 @@ ${imagesListArr.join('')}
 galleryList.insertAdjacentHTML('beforeend', listTemplate);
 
 
+const container = document.querySelector('.gallery');
+container.addEventListener('click', onClick);
+function onClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
 
+const largeImageUrl = event.target.dataset.source;
+  console.log('URL великого зображення:', largeImageUrl);
+    
+} 
