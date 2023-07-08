1aa970b27edb466f010e8d30917eac05a95c335a
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-08 22:46:02 +0200

    02-markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const imagesListArr = galleryItems.map((image) => {
+return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+})
+const listTemplate = `
+${imagesListArr.join('')}
+ `;
+galleryList.insertAdjacentHTML('beforeend', listTemplate);
+
