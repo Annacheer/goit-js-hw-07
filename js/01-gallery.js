dc9d70e257933b6d8b48b7f45603967ea787ed39
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-07-10 00:07:37 +0200

    Update Task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,40 +3,45 @@ import { galleryItems } from './gallery-items.js';
 const galleryList = document.querySelector('.gallery');
 
 const imagesListArr = galleryItems.map((image) => {
-return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image"
+  return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image"
       src="${image.preview}"
       data-source="${image.original}"
       alt="${image.description}"
     />
   </a>
-</li>`
-})
+</li>`;
+});
 const listTemplate = `
 ${imagesListArr.join('')}
  `;
 galleryList.insertAdjacentHTML('beforeend', listTemplate);
 
-
-const container = document.querySelector('.gallery');
-container.addEventListener('click', onClick);
+galleryList.addEventListener('click', onClick);
 
 let instance;
 
 function onClick(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `<img src="${largeImageUrl}" alt="${event.target.alt}">`,
+    {
+      onShow: () => {
+        document.addEventListener('keyup', closeGallery);
+      },
+      onClose: () => {
+        document.removeEventListener('keyup', closeGallery);
+      },
     }
+  );
 
-const largeImageUrl = event.target.dataset.source;    
- instance = basicLightbox.create(`
-    <img src="${largeImageUrl}" alt="${event.target.alt}">
-`);
+  instance.show();
+}
 
-instance.show()
-} 
-
-document.addEventListener('keyup', closeGallery);
 function closeGallery(event) {
   if (event.code === 'Escape') {
     instance.close();
